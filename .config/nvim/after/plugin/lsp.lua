be3dacf4ea92c18c4ba03256e95202ed3a860215
Julian One <38872371+julian-one@users.noreply.github.com>
2023-08-20 08:22:26 -0700

    nvim

---
+++ .config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,10 @@
+local lsp = require('lsp-zero').preset('recommended')
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+
+lsp.setup()
---
+++ .config/nvim/lua/julian-one/init.lua
@@ -0,0 +1,2 @@
+require("julian-one.remap")
+require("julian-one.set")
